4f17298827af210bcad7e01a8d341433e40779e8
cindy-hj <hj0304hj@gmail.com>
2023-03-02 17:00:31 +0900

    2023 03 02 17:00
    jsp 게시판 만들기

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -14,11 +14,11 @@ public class UserDAO {
 	public UserDAO() {
 		try {
 			String dbDriver = "com.mysql.cj.jdbc.Driver";
-			String dbURL = "jdbc:mysql://localhost:3306/userbbs";
+			String dbURL = "jdbc:mysql://localhost:3306/bbs";
 			String dbID = "userbbs";
 			String dbPassword = "0000";
 			Class.forName(dbDriver);
-			conn = DriverManager.getConnection(dbURL);
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -42,5 +42,21 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 		return -2; // 오류
+	}
+	
+	public int join(User user) {
+		String sql = "insert into user values (?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
 	}
 }
